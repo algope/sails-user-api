e6da56d385b350bc90c335b2d1ebb4e3724481b7
Alejandro G <alejandro@veraw56-128.wi-fi.upv.es>
2015-10-22 18:24:29 +0200

    Local disk for testing

--- config/env/development.js
+++ config/env/development.js
@@ -18,7 +18,7 @@ module.exports = {
      ***************************************************************************/
 
     models: {
-        connection: 'devMySQL'
+        connection: 'localDiskDb'
     }
 
 
--- config/env/testing.js
+++ config/env/testing.js
@@ -19,7 +19,7 @@ module.exports = {
     port: 80,
 
     models: {
-        connection: 'testMySQL'
+        connection: 'localDiskDb'
     }
 
 
